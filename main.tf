b13f92da0c9c41aa48a3b0e5e963038727bf30dd
akshasri <akshay.srivastava@appdynamics.com>
2020-02-07 17:37:02 +0530

    Changing security group

--- main.tf
+++ main.tf
@@ -7,11 +7,5 @@ provider "aws" {
 resource "aws_instance" "aws_btd" {
   ami = "ami-082b5a644766e0e6f"
   instance_type = "t2.micro"
-
-
-  #Reading the subnet_id from variables.tf
-  subnet_id = "subnet-55f7f62d"
-
-  #Security group
-  #security_groups = ["sg-ccb3ebb"]
+  security_groups   = ["extensions-buildAgents"]
 }
